7bb66b8988b8c49650cff704adfb418bcef817d1
Kyle Saburao <kyle.saburao@gmail.com>
2026-01-31 15:21:10 -0800

    fast code

--- hi.js
+++ hi.js
@@ -1,12 +1,24 @@
-const f = (v, val) => {
-    return v + val;
+const func = async (worker) => {
+    console.log('START WORKER', worker);
+    const f = (v, val) => {
+        return v + val;
+    };
+
+    let x = 0;
+    while (x === 0) {
+        x = f(x, 1);
+        x = f(x, -1);
+        await new Promise(resolve => setTimeout(resolve, 0));
+    }
+
+    console.log('DONE WORKER', worker);
+    return 1;
 };
 
-let x = 0;
-while (x === 0) {
-    x = f(x, 1);
-    x = f(x, -1);
-    console.log(x);
-}
+const promises = Array.from({ length: 1000 }).map((_, i) => {
+    return func(i);
+});
 
-console.log('done');
+Promise.all(promises).then(() => {
+    console.log('All done');
+});
